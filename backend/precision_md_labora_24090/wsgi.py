f1d92d3c27e9d52f3299f7f20ab67dfa9bedd5d5
Crowdbotics <team@crowdbotics.com>
2021-01-26 13:55:50 +0000

    Update models

--- backend/precision_md_labora_24090/wsgi.py
+++ backend/precision_md_labora_24090/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'precision_md_labora_24090.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "precision_md_labora_24090.settings")
 
 application = get_wsgi_application()
